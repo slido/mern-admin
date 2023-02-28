6fc6b55fefd26941a3e45db52378e7bf6aee8aa9
dkoletic <denis.koletic@q-software.com>
2023-02-28 21:15:08 +0100

    fix

--- routes/products/index.js
+++ routes/products/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Product = require("../models/Product");
+const Product = require("../models/product");
 
 //CREATE PRODUCT
 router.post("/", async (req, res) => {
