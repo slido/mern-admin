b93c9a28c7daef2bf3c4a6aac6479a0e0286826e
dkoletic <denis.koletic@q-software.com>
2023-02-28 22:30:56 +0100

    changes

--- routes/middlewares/openai.js
+++ routes/middlewares/openai.js
@@ -1,6 +1,6 @@
 const OpenAI = require('openai-api');
 
-const OPENAI_API_KEY = "DrD47nKpKGkTiY9Shw6dJFkblB3Te2FKiJJh7S5QbM0H1WbK-ks";
+const OPENAI_API_KEY = "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";
 const openai = new OpenAI(OPENAI_API_KEY);
 
 module.exports = openai
